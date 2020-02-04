da6da63cd63181df58f76449892424fc3e0699c2
parmeped <parmeggianipedro@gmail.com>
2020-02-04 00:17:04 -0300

    Got to deploy, but returns internal server error

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/SolarSystem
 
 go 1.13
 
-require github.com/gin-gonic/gin v1.5.0
+require (
+	github.com/gin-gonic/gin v1.5.0
+	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
+)
--- main.go
+++ main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
-
-	sol "github.com/SolarSystem/pkg/system"
-	"github.com/SolarSystem/pkg/utl/config"
-	"github.com/SolarSystem/pkg/service"
-	"github.com/SolarSystem/pkg/api"
-	repo "github.com/SolarSystem/pkg/repository"
+	"fmt"
+	"net/http"
 )
 
 func main() {
 
-	cfg := config.Load()	
+	// cfg := config.Load()
 
-	// Load initial planets
-	DB := repo.New()
-	DB.SolarSystem = sol.New(cfg.Planets, cfg)	
-	
-	service := service.New(DB)
-	r := api.SetupRouter(service)
+	// // Load initial planets
+	// DB := repo.New()
+	// DB.SolarSystem = sol.New(cfg.Planets, cfg)
 
-	r.Run()
+	// service := service.New(DB)
+	// r := api.SetupRouter(service)
+	// r.Run()
+	http.HandleFunc("/", handle)
+
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello world!")
 }
